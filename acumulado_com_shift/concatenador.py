62f7e5bf245c01ca6947b15deb76046f88fe776e
thomaz <thomazrebellomiranda@gmail.com>
2023-06-18 20:10:14 -0700

    concatenador2

--- acumulado_com_shift/concatenador.py
+++ acumulado_com_shift/concatenador.py
@@ -11,8 +11,8 @@ for filename in os.listdir(directory):
         df = pd.read_csv(os.path.join(directory, filename))
         df.drop('Unnamed: 0', inplace=True, axis=1)
 
-        df['filename'] = Path(filename).stem
+        df.columns = [f'{x}_{Path(filename).stem}' for x in df.columns]
 
-        all_data = pd.concat([df, all_data])
+        all_data = pd.concat([df, all_data], axis=1)
 
 all_data.to_pickle(r'C:\Users\thoma\OneDrive\Documentos\GitHub\DS-INF1032\acumulado_com_shift\concatenado.zip')
